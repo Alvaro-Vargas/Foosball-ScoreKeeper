371ca1f933b6279e7318eab1eb7f43cac670afcd
Alvaro-Vargas <alvaro.pvj@gmail.com>
2019-08-21 17:32:10 -0300

    Working on howler

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
-var tnt1 = document.getElementById("tnt1");
+var taunt = new Howl({
+  src: ["./sounds/taunt.mp3"]
+});
 
-tnt1.onclick = () => {
-  console.log("play sound");
+var goal = new Howl({
+  src: ["./sounds/goal.mp3"]
+});
+
+const gl1 = document.getElementById("gl1");
+
+const tn1 = document.getElementById("tn1");
+
+const gl2 = document.getElementById("gl2");
+
+const tn2 = document.getElementById("tn2");
+
+gl1.onclick = () => {
+  goal.play();
+};
+tn1.onclick = () => {
+  taunt.play();
+};
+gl2.onclick = () => {
+  goal.play();
+};
+tn2.onclick = () => {
+  taunt.play();
 };
